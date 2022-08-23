fedde9ed2e58b8a58992a74ecfca6c907fa8bace
Hankerman <stalker-2204@mail.ru>
2022-08-23 23:18:59 +0300

    Добавьте файлы проекта.

---
+++ Hw1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание 1
+            //int num;
+            //Console.WriteLine("Введите число в диапазоне от 1 до 100");
+            //num = Convert.ToInt32(Console.ReadLine());
+            //if (num >= 1 && num <= 100)
+            //{
+            //    if (num % 3 == 0 && num % 5 == 0)
+            //    {
+            //        Console.WriteLine("Fizz Buzz");
+            //    }else if (num % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }else if (num % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ошибка");
+            //}
+
+
+            // задание 2
+            //int percent;
+            //double number;
+            //Console.WriteLine("Введите число и процент");
+            //number  = Convert.ToDouble(Console.ReadLine());
+            //percent = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Получилось " + ((number / 100) * percent));
+
+
+            //задание 3
+            //string number;
+            //Console.WriteLine("Введите 4 числа");
+            //number = Console.ReadLine();
+            //number +=Console.ReadLine();
+            //number += Console.ReadLine();
+            //number += Console.ReadLine();
+            //Console.WriteLine(number);
+
+            //задание4
+
+
+
+        }
+    }
+}
